82a5e33953c49b7f6311597351604f156698a4d7
alrago24 <alinson_04@hotmail.com>
2025-09-27 13:51:01 -0500

    Fase 4: Menú interactivo implementado

--- 1090537019-Alison-Diaz/app.js
+++ 1090537019-Alison-Diaz/app.js
@@ -1,14 +1,14 @@
-let nombreUsuario = prompt("Hola, por favor ingresa tu nombre:");
+let nombreUsuario = prompt("Hola, Bienvenido a nuestro simulador de finanzas personales,por favor ingresa tu nombre:");
 let saldo = 1000;
 let gastosTotales = 0;
 
 function mostrarBalance() {
     console.log("Hola " + nombreUsuario + ", tu saldo actual es: $" + saldo + ". Has gastado: $" + gastosTotales);
-    
-}
-mostrarBalance();
 
-const registrarIngreso = function(montoIngreso) {
+}
+// mostrarBalance();
+
+const registrarIngreso = function (montoIngreso) {
     saldo = saldo + montoIngreso;
     console.log(`Ingreso de $ ${montoIngreso} registrado. Nuevo saldo: $${saldo}`);
 }
@@ -18,16 +18,16 @@ const registrarGasto = (montoGasto) => {
         saldo = saldo - montoGasto;
         gastosTotales = gastosTotales + montoGasto;
         console.log(`Transacción exitosa por $ ${montoGasto} registrada. Nuevo saldo: $${saldo}`);
-    }   else {
+    } else {
         console.log("Fondos insuficientes.");
     }
 }
 
-registrarIngreso(5000);
+// registrarIngreso(5000);
 
-registrarGasto(200);
-registrarGasto(200);
-mostrarBalance();
+// registrarGasto(200);
+// registrarGasto(200);
+// mostrarBalance();
 
 
 const simularSemana = () => {
@@ -46,7 +46,7 @@ function diagnosticoFinanciero() {
     };
 
     switch (true) {
-        case (gastosTotales > 1000 && gastosTotales <= 1500):
+        case (gastosTotales > 1000):
             console.log("Has gastado mucho");
             break;
         case (gastosTotales > 700 && gastosTotales <= 1000):
@@ -61,7 +61,42 @@ function diagnosticoFinanciero() {
     }
 }
 
-simularSemana();
+// simularSemana();
 
 
-diagnosticoFinanciero();
+// diagnosticoFinanciero();
+
+
+function iniciarApp() {
+    while (true) {
+        let opcion = prompt("Menu de gastos:\n1. Registrar un Ingreso\n2. Registrar un Gasto\n3. Simular una Semana de Gastos\n4. Ver Diagnóstico Financiero\n5. Mostrar Balance Actual \n6. Salir");
+        switch (opcion) {
+        case ("1"):
+            let ingreso = parseInt(prompt("Ingresa el monto del ingreso:"));
+            registrarIngreso(ingreso);
+            break;
+        case ("2"):
+            let gasto = parseInt(prompt("Ingresa el monto de tu gasto:"));
+            registrarGasto(gasto);
+        break;
+        case ("3"):
+            simularSemana();
+            break;
+        case ("4"):
+            diagnosticoFinanciero();
+            break;
+        case ("5"):
+            mostrarBalance();
+            break;
+        case ("6"):
+            console.log("Fue un placer ayudarte. Hasta la proxima.");
+            alert("Fue un placer ayudarte. Hasta la proxima.")
+            return;
+        default:
+            alert("opción no valida");
+            break;
+        }
+    }
+}
+
+iniciarApp();
