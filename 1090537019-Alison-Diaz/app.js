a1264fb867f8248441c6bad61f54c66c44eb0d83
alrago24 <alinson_04@hotmail.com>
2025-09-27 12:06:03 -0500

    Fase 3: Añadida simulación semanal y diagnóstico financiero

--- 1090537019-Alison-Diaz/app.js
+++ 1090537019-Alison-Diaz/app.js
@@ -23,8 +23,45 @@ const registrarGasto = (montoGasto) => {
     }
 }
 
-registrarIngreso(500);
+registrarIngreso(5000);
 
 registrarGasto(200);
-registrarGasto(1300);
+registrarGasto(200);
 mostrarBalance();
+
+
+const simularSemana = () => {
+    for (let i = 0; i < 7; i++) {
+        registrarGasto(50);
+    }
+};
+
+function diagnosticoFinanciero() {
+    if (saldo > 2000) {
+        console.log("Tu salud financiera es excelente");
+    } else if (saldo < 2000 && saldo > 1000) {
+        console.log("Tu salud financiera puede mejorar");
+    } else {
+        console.log("Alerta: Fondos bajos.");
+    };
+
+    switch (true) {
+        case (gastosTotales > 1000 && gastosTotales <= 1500):
+            console.log("Has gastado mucho");
+            break;
+        case (gastosTotales > 700 && gastosTotales <= 1000):
+            console.log("Tus gastos son moderados");
+            break;
+        case (gastosTotales < 500):
+            console.log("Has sido muy cuidadoso");
+            break;
+        default:
+            console.log("No se han registrado gastos.");
+            break;
+    }
+}
+
+simularSemana();
+
+
+diagnosticoFinanciero();
