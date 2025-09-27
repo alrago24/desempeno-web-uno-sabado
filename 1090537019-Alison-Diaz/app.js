95142df1c647254ba442e41c577d8f8074cdebf5
alrago24 <alinson_04@hotmail.com>
2025-09-27 11:31:54 -0500

    Fase 2: Implementadas funciones para ingresos y gastos

--- 1090537019-Alison-Diaz/app.js
+++ 1090537019-Alison-Diaz/app.js
@@ -6,5 +6,25 @@ function mostrarBalance() {
     console.log("Hola " + nombreUsuario + ", tu saldo actual es: $" + saldo + ". Has gastado: $" + gastosTotales);
     
 }
+mostrarBalance();
 
+const registrarIngreso = function(montoIngreso) {
+    saldo = saldo + montoIngreso;
+    console.log(`Ingreso de $ ${montoIngreso} registrado. Nuevo saldo: $${saldo}`);
+}
+
+const registrarGasto = (montoGasto) => {
+    if (montoGasto <= saldo) {
+        saldo = saldo - montoGasto;
+        gastosTotales = gastosTotales + montoGasto;
+        console.log(`Transacción exitosa por $ ${montoGasto} registrada. Nuevo saldo: $${saldo}`);
+    }   else {
+        console.log("Fondos insuficientes.");
+    }
+}
+
+registrarIngreso(500);
+
+registrarGasto(200);
+registrarGasto(1300);
 mostrarBalance();
